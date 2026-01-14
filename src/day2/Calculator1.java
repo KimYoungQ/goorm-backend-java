05edfe9f6582b80bb9b4c38bcce4866ddd24372a
김영규 <dudeb0206@daum.net>
2026-01-14 21:44:21 +0900

    git 초기화 및 2일차 내용까지

---
+++ src/day2/Calculator1.java
@@ -0,0 +1,39 @@
+package day2;
+
+import java.util.Scanner;
+
+public class Calculator1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("첫번째 숫자: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("연산자 (+, -, *, /): ");
+        String operator = scanner.next();
+
+        System.out.println("두번쨰 숫자: ");
+        double num2 = scanner.nextDouble();
+
+        double result = 0;
+
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+        }
+
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+
+        scanner.close();
+    }
+}
