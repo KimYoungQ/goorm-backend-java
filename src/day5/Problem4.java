64d4adb34aa553b635bc2b2dce843d79480526ab
김영규 <dudeb0206@daum.net>
2026-01-17 16:39:11 +0900

    day5

---
+++ src/day5/Problem4.java
@@ -0,0 +1,17 @@
+package day5;
+
+public class Problem4 {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10};
+        int sum = 0;
+
+        for (int i = 1; i <= arr.length; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            sum += arr[i-1];    // 18
+        }
+
+        System.out.println("sum = " + sum);
+    }
+}
